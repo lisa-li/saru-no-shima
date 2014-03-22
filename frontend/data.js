6f7e18d61bcf4c1d40e5a4e3e142b552e564d2b8
wilddamon <wilddamon@gmail.com>
2014-03-22 15:31:08 +1100

    add extra markers

--- frontend/data.js
+++ frontend/data.js
@@ -35,5 +35,39 @@ var quests = [
       wrong: [{id: 4, english: 'Merry Christmas!', japanese: 'メリ　クリスマス。'}]
     }
   ]
-}
+},
+{
+  id: '3',
+  name: 'hello',
+  description: 'Greeting a friend.',
+  character: 'Alex',
+  location: [ 35.746235, 140.371714 ],
+  background: 'garden',
+  questions: [
+    {
+      question: {english: "Hello", japanese: 'こんにちは'},
+      correct: [{id: 1, english: 'Hello', japanese: 'こんにちは'}],
+      neutral: [
+        {id: 2, english: 'Um', japanese: 'ええと'},
+        {id: 3, english: 'Er', japanese: 'あのう'}
+      ],
+      wrong: [{id: 4, english: 'Go away', japanese: 'かってにしろ'}]
+    }
+  ]
+},
+{
+  id: '2',
+  name: 'train ticket',
+  description: 'Buying a train ticket',
+  character: 'Sam',
+  location: [ 35.758667, 140.414028 ],
+  questions: [
+    {
+      question: "How may I help you?",
+      correct: ["I'd like to buy a train ticket"],
+      neutral: ["Train ticket?", "Buy ticket?"],
+      wrong: ["I want to buy a fish"]
+    }
+  ]
+},
 ];
